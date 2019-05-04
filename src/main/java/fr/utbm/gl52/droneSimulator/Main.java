6d0aba6209502d06e7c3cdee3ae112c5802bfdff
Rémi Cabezas <remi.cabezas@gmx.com>
2019-05-04 23:32:59 +0200

    Revert "Test bouncing ball."
    This reverts commit c4ff45a2692b40610c534472c7f17fd0f85e5d97.

--- src/main/java/fr/utbm/gl52/droneSimulator/Main.java
+++ src/main/java/fr/utbm/gl52/droneSimulator/Main.java
@@ -1,11 +1,11 @@
 package fr.utbm.gl52.droneSimulator;
 
-import fr.utbm.gl52.droneSimulator.service.DbDroneService;
-import fr.utbm.gl52.droneSimulator.service.entity.DbDrone;
-
 public class Main {
+
     public static void main(String[] args) {
         GuiMain.main(args);
     }
 
 }
+
+
