d0ea779111a0eb21c64a74468a9bfc9ceeb2d35e
Valentin <valentin.sourice@valink.fr>
2019-05-07 09:40:28 +0200

    improve exceptionHandlers, fix areas, move drone

--- src/main/java/fr/utbm/gl52/droneSimulator/model/exception/OutOfMainAreaException.java
+++ src/main/java/fr/utbm/gl52/droneSimulator/model/exception/OutOfMainAreaException.java
@@ -1,6 +1,6 @@
 package fr.utbm.gl52.droneSimulator.model.exception;
 
-public class OutOfMainAreaException extends RuntimeException {
+public class OutOfMainAreaException extends Exception {
     public OutOfMainAreaException(String message){
         super("OutOfMainAreaException: "+message);
     }
