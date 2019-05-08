826fcc838566538d46b82e903674634119f93ed5
Rémi Cabezas <remi.cabezas@gmx.com>
2019-05-08 15:20:29 +0200

    Ability to add drones in parameter window.

--- src/main/java/fr/utbm/gl52/droneSimulator/model/Simulation.java
+++ src/main/java/fr/utbm/gl52/droneSimulator/model/Simulation.java
@@ -14,6 +14,8 @@ public class Simulation {
     private static Float speed;
     private static Integer parcelNumber;
     private static Integer droneNumber;
+    private static Float[] droneWeightCapacity = new Float[2];
+    private static Integer[] droneBatteryCapacity = new Integer[2];
 
     private static final Float mainAreaWidth = 16000f;
     private static final Float mainAreaHeight = 9000f;
@@ -23,6 +25,10 @@ public class Simulation {
         speed = 17f; // assez petit pour un déplacement qui semble naturel (contigu et non sacadé)
         droneNumber = 10;
         parcelNumber = 10;
+        droneWeightCapacity[0] = 0.1f;
+        droneWeightCapacity[1] = 20f;
+        droneBatteryCapacity[0] = 5;
+        droneBatteryCapacity[1] = 55;
     }
 
     public static void setSpeed(Float f) {
@@ -155,5 +161,12 @@ public class Simulation {
     }
     public static void setParcels(ArrayList<Parcel> parcels) {
         parcels = parcels;
+    }
+    public static Float[] getDroneWeightCapacity() {
+        return droneWeightCapacity;
+    }
+
+    public static Integer[] getDroneBatteryCapacity() {
+        return droneBatteryCapacity;
     }
 }
