b33eceb31f944babbc461338c2c576ba39bc0a3f
Rémi Cabezas <remi.cabezas@gmx.com>
2019-06-05 19:51:31 +0200

    Start to add comments on the code.

--- src/main/java/fr/utbm/gl52/droneSimulator/model/Area.java
+++ src/main/java/fr/utbm/gl52/droneSimulator/model/Area.java
@@ -12,6 +12,14 @@ public class Area extends SimulationElement {
     public Area() {
     }
 
+    /**
+     * Constructor of a new area
+     *
+     * @param x abscissa of the area
+     * @param y ordinate of the area
+     * @param width width of the area
+     * @param height height of the area
+     */
     public Area(Float x, Float y, Float width, Float height) {
         try {
             setX(x);
@@ -22,6 +30,12 @@ public class Area extends SimulationElement {
             e.printStackTrace();
         }
     }
+
+    /**
+     * Generate an area width random position within the main area and random size
+     *
+     * @return random area
+     */
     public static Area createRandomized() {
         Area area = new Area();
         try {
@@ -32,24 +46,51 @@ public class Area extends SimulationElement {
         return area;
     }
 
+    /**
+     * Randomize the size and the position of the area
+     *
+     * @throws OutOfMainAreaException The random position is out of the main area or the size exceeds the main area
+     * @throws NotSupportedValueException Position or size are negative
+     */
     public void randomize() throws OutOfMainAreaException, NotSupportedValueException {
         setRandCoord(Simulation.getMainArea());
         setRandSize();
     }
 
+
+    /**
+     * Randomize the size of the area
+     *
+     * @throws OutOfMainAreaException The size exceeds the main area
+     * @throws NotSupportedValueException size is negative
+     */
     private void setRandSize() throws OutOfMainAreaException, NotSupportedValueException {
         setRandWidth();
         setRandHeight();
     }
 
+    /**
+     * Randomize the width of the area
+     *
+     * @throws OutOfMainAreaException The width exceeds the main area boundaries
+     * @throws NotSupportedValueException The width is negative
+     */
     private void setRandWidth() throws OutOfMainAreaException, NotSupportedValueException {
         Float randomPossibleWidth = RandomHelper.getRandFloat(0f, Simulation.getMainArea().getWidth() - getX());
         setWidth(randomPossibleWidth);
     }
+
+    /**
+     * Randomize the height of the area
+     *
+     * @throws OutOfMainAreaException The height exceeds the main area boundaries
+     * @throws NotSupportedValueException The height is negative
+     */
     private void setRandHeight() throws OutOfMainAreaException, NotSupportedValueException {
         Float randomPossibleHeight = RandomHelper.getRandFloat(0f, Simulation.getMainArea().getHeight() - getY());
         setHeight(randomPossibleHeight);
     }
+
 
     public Float getWidth() {
         return width;
@@ -59,6 +100,13 @@ public class Area extends SimulationElement {
         return height;
     }
 
+    /**
+     * Set the with of the Area
+     *
+     * @param w width in meters
+     * @throws NotSupportedValueException The width is negative
+     * @throws OutOfMainAreaException The width exceeds the main area boundaries
+     */
     public void setWidth(Float w) throws NotSupportedValueException, OutOfMainAreaException{
         if (w <= 0)
             throw new NotSupportedValueException("width can't be <= 0");
@@ -68,6 +116,13 @@ public class Area extends SimulationElement {
             width = w;
     }
 
+    /**
+     * Set the height of the Area
+     *
+     * @param h width in meters
+     * @throws NotSupportedValueException The height is negative
+     * @throws OutOfMainAreaException The height exceeds the main area boundaries
+     */
     public void setHeight(Float h) throws NotSupportedValueException, OutOfMainAreaException{
         if (h <= 0)
             throw new NotSupportedValueException("height can't be <= 0");
@@ -77,10 +132,24 @@ public class Area extends SimulationElement {
             height = h;
     }
 
+    /**
+     * Check if the area exceeds the width boundary of the main area
+     *
+     * @param x width of the area in meters
+     *
+     * @return true if x is in boundary
+     */
     public Boolean isInAreaXBoundary(Float x) {
         return (x < getX() )|| (x > (getX() + getWidth()));
     }
 
+    /**
+     * Check if the area exceeds the height boundary of the main area
+     *
+     * @param y height of the area in meters
+     *
+     * @return true if y is in boundary
+     */
     public Boolean isInAreaYBoundary(Float y) {
         return (y < getY()) || (y > (getY() + getHeight()));
     }
--- src/main/java/fr/utbm/gl52/droneSimulator/model/SimulationElementInterface.java
+++ src/main/java/fr/utbm/gl52/droneSimulator/model/SimulationElementInterface.java
@@ -1,8 +1,32 @@
 package fr.utbm.gl52.droneSimulator.model;
 
 public interface SimulationElementInterface {
+
+    /**
+     * Get the abscissa of the element
+     *
+     * @return abscissa of the element
+     */
     Float getX();
+
+    /**
+     * Get the ordinate of the element
+     *
+     * @return ordinate of the element
+     */
     Float getY();
+
+    /**
+     * Get the width of the element
+     * .
+     * @return width of the element
+     */
     Float getWidth();
+
+    /**
+     * Get the height of the element
+     * .
+     * @return height of the element
+     */
     Float getHeight();
 }
