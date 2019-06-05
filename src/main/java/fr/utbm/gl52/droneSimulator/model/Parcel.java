f298e31272f197488ad3ccfc047d3693092f40bb
Rémi Cabezas <remi.cabezas@gmx.com>
2019-06-05 22:10:32 +0200

    Adding new comments on the model

--- src/main/java/fr/utbm/gl52/droneSimulator/model/Parcel.java
+++ src/main/java/fr/utbm/gl52/droneSimulator/model/Parcel.java
@@ -8,7 +8,6 @@ import java.util.Date;
 
 import static fr.utbm.gl52.droneSimulator.view.SimulationWindowView.removeParcelGraphicIfExists;
 import static fr.utbm.gl52.droneSimulator.view.graphicElement.ParcelGraphicElement.findParcelGraphicWithParcelCoord;
-import static fr.utbm.gl52.droneSimulator.view.graphicElement.ParcelGraphicElement.removeParcelGraphicAtCoord;
 
 public class Parcel extends CenteredAndSquaredSimulationElement {
 
@@ -18,17 +17,32 @@ public class Parcel extends CenteredAndSquaredSimulationElement {
     private Float[] destCoord = new Float[2];
     private boolean isInJourney = false;
 
+    /**
+     * Construct a nex parcel with the given Id
+     *
+     * @param id Id of the parcel
+     */
     public Parcel(Integer id) {
         super(id, .5f);
         popTime = new Date();
     }
 
+    /**
+     * Create a parcel with a random weight, position and destination
+     *
+     * @param id Id of th parcel
+     *
+     * @return Parcel
+     */
     public static Parcel createRandomized(Integer id) {
         Parcel parcel = new Parcel(id);
         parcel.randomize();
         return parcel;
     }
 
+    /**
+     * Randomize the weight, position and destination of the parcel
+     */
     public void randomize() {
         setRandWeight();
         try {
@@ -44,6 +58,12 @@ public class Parcel extends CenteredAndSquaredSimulationElement {
         }
     }
 
+    /**
+     * Load on the drone the parcel that is at the given coordinates
+     *
+     * @param coord coordinates of the parcel
+     * @param drone drone that loads the parcel
+     */
     public static void loadParcelAtCoord(Float[] coord, Drone drone){
         ParcelGraphicElement parcelToRemove;
 
@@ -61,28 +81,58 @@ public class Parcel extends CenteredAndSquaredSimulationElement {
         });
     }
 
+    /**
+     * Set a rand weight to the parcel
+     */
     public void setRandWeight() {
         setWeight(RandomHelper.getRandFloat(0f, 20f));
     }
 
+    /**
+     * Set a rand abscissa within the area boundaries
+     *
+     * @param area Area in which the abscissa should be set
+     *
+     * @throws OutOfMainAreaException The abscissa exceeds the area boundaries
+     */
     protected void setRandDestX(Area area) throws OutOfMainAreaException {
 //        setDestX(RandomHelper.getRandFloat(area.getX(), (area.getX() + area.getWidth())));
 //        setDestX(area.getWidth()/2);
         setDestX(RandomHelper.getRandFloat(area.getX() + getWidth() / 2, (area.getX() + area.getWidth()) - getWidth() / 2));
     }
 
+    /**
+     * Set a rand ordinates within the area boundaries
+     *
+     * @param area Area in which the ordinates should be set
+     *
+     * @throws OutOfMainAreaException The ordinates exceeds the area boundaries
+     */
     protected void setRandDestY(Area area) throws OutOfMainAreaException {
 //        setDestY(RandomHelper.getRandFloat(area.getY(), (area.getY()+ area.getHeight())));
 //        setDestY(area.getHeight()/2);
         setDestY(RandomHelper.getRandFloat(area.getY() + getHeight() / 2, (area.getY() + area.getHeight()) - getHeight() / 2));
     }
 
+    /**
+     * Set random coordinates within the area boundaries
+     *
+     * @param area Area in which the coordinates should be set
+     *
+     * @throws OutOfMainAreaException The coordinates exceeds the area boundaries
+     */
     public void setRandDestCoord(Area area) throws OutOfMainAreaException {
         setRandDestX(area);
         setRandDestY(area);
     }
 
     /* getteurs et setteurs triviaux */
+
+    /**
+     * Get the pop time on the area of the parcel
+     *
+     * @return pop time of the drone
+     */
     public Date getPopTime() {
         return popTime;
     }
@@ -92,21 +142,32 @@ public class Parcel extends CenteredAndSquaredSimulationElement {
     public Float getWeight() {
         return weight;
     }
+
+    /**
+     * Set the weight of the parcel
+     *
+     * @param weight Weight in kilograms
+     */
     public void setWeight(Float weight) {
         this.weight = weight;
     }
 
-    public String toString() {
-        String s =
-                super.toString() +
-                        "weight: " + getWeight() + System.getProperty("line.separator");
-        return s;
-    }
-
+    /**
+     * Get the destination coordinates of the parcel
+     *
+     * @return Array [x,y]
+     */
     public Float[] getDestCoord() {
         return destCoord;
     }
 
+    /**
+     * Set the abscissa of the element
+     *
+     * @param x the abscissa position in meters
+     *
+     * @throws OutOfMainAreaException x is not in the main area
+     */
     public void setDestX(float x) throws OutOfMainAreaException {
         if (Simulation.getMainArea().isInAreaXBoundary(x))
             throw new OutOfMainAreaException("x out of mainArea boundary : " + x);
@@ -114,6 +175,13 @@ public class Parcel extends CenteredAndSquaredSimulationElement {
             destCoord[0] = x;
     }
 
+    /**
+     * Set the ordinate of the element
+     *
+     * @param y the ordinate position in meters
+     *
+     * @throws OutOfMainAreaException x is not in the main area
+     */
     public void setDestY(float y) throws OutOfMainAreaException {
         if (Simulation.getMainArea().isInAreaYBoundary(y))
             throw new OutOfMainAreaException("y out of mainArea boundary : " + y);
@@ -121,10 +189,20 @@ public class Parcel extends CenteredAndSquaredSimulationElement {
             destCoord[1] = y;
     }
 
+    /**
+     * Check if the parcel is loaded on a drone
+     *
+     * @return True if the parcel is in journey
+     */
     public boolean isInJourney() {
         return isInJourney;
     }
 
+    /**
+     * Set parcel in journey
+     *
+     * @param inJourney True if the parcel is in journey
+     */
     public void setInJourney(boolean inJourney) {
         isInJourney = inJourney;
     }
--- src/main/java/fr/utbm/gl52/droneSimulator/model/SimulationElement.java
+++ src/main/java/fr/utbm/gl52/droneSimulator/model/SimulationElement.java
@@ -21,39 +21,91 @@ public abstract class SimulationElement implements SimulationElementInterface {
         return areaReturn;
     }
 
+    /**
+     * Set a rand abscissa within the area boundaries
+     *
+     * @param area Area in which the abscissa should be set
+     *
+     * @throws OutOfMainAreaException The abscissa exceeds the area boundaries
+     */
     protected void setRandX(Area area) throws OutOfMainAreaException {
 //        setX(RandomHelper.getRandFloat(area.getX(), (area.getX() + area.getWidth())));
         setX(area.getWidth()/2);
     }
 
+    /**
+     * Set a rand ordinate within the area boundaries
+     *
+     * @param area Area in which the ordinate should be set
+     *
+     * @throws OutOfMainAreaException The ordinate exceeds the area boundaries
+     */
     protected void setRandY(Area area) throws OutOfMainAreaException {
 //        setY(RandomHelper.getRandFloat(area.getY(), (area.getY() + area.getHeight())));
         setY(area.getHeight()/2);
     }
 
+    /**
+     * Set Rand coordinates in a given area
+     *
+     * @param area The area i which the element should be placed
+     *
+     * @throws OutOfMainAreaException The element is out of the main area
+     */
     public void setRandCoord(Area area) throws OutOfMainAreaException {
         setRandX(area);
         setRandY(area);
     }
 
     /* getteurs et setteurs triviaux */
+
+    /**
+     * Get the coordinate array [x,Y]
+     *
+     * @return The coordinates
+     */
     public Float[] getCoord() {
         return coord;
     }
 
+    /**
+     * Set the coordinates of the element
+     *
+     * @param x the abscissa position in meters
+     * @param y the ordinate position in meters
+     *
+     * @throws OutOfMainAreaException x or y are not in the main area
+     */
     public void setCoord(Float x, Float y) throws OutOfMainAreaException{
         setX(x);
         setY(y);
     }
 
+    /**
+     * Get the abscissa of the element
+     *
+     * @return abscissa in meters
+     */
     public Float getX() {
         return coord[0];
     }
 
+    /**
+     * get the ordinate of the element
+     *
+     * @return ordinate in meter
+     */
     public Float getY() {
         return coord[1];
     }
 
+    /**
+     * Set the abscissa of the element
+     *
+     * @param x the abscissa position in meters
+     *
+     * @throws OutOfMainAreaException x is not in the main area
+     */
     public void setX(Float x) throws OutOfMainAreaException {
         if (Simulation.getMainArea().isInAreaXBoundary(x))
             throw new OutOfMainAreaException("x out of mainArea boundary : " + x);
@@ -61,6 +113,13 @@ public abstract class SimulationElement implements SimulationElementInterface {
             coord[0] = x;
     }
 
+    /**
+     * Set the coordinates of the element
+     *
+     * @param y the ordinate position in meters
+     *
+     * @throws OutOfMainAreaException y is not in the main area
+     */
     public void setY(Float y) throws OutOfMainAreaException {
         if (Simulation.getMainArea().isInAreaYBoundary(y))
             throw new OutOfMainAreaException("y out of mainArea boundary : " + y);
@@ -68,6 +127,7 @@ public abstract class SimulationElement implements SimulationElementInterface {
             coord[1] = y;
     }
 
+    @Override
     public String toString() {
         String s = "coords: [" + getX() + "," + getY() + "]" + System.getProperty("line.separator");
         return s;
