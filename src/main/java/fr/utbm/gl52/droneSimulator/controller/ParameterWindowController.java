5b38d97b287ee861b6ef7552908506c73a80ceea
Rémi Cabezas <remi.cabezas@gmx.com>
2019-06-08 19:11:52 +0200

    Add a popup if no charging station is added in custom mode.
    Add commentary in parametersWindowController

--- src/main/java/fr/utbm/gl52/droneSimulator/controller/ParameterWindowController.java
+++ src/main/java/fr/utbm/gl52/droneSimulator/controller/ParameterWindowController.java
@@ -37,6 +37,11 @@ public class ParameterWindowController{
     private Integer droneIdIncrement = 0;
     private Integer chargingStationIdIncrement = 0;
 
+    /**
+     * Set the next action as a drone placement.
+     *
+     * @param ae The event that has triggered this action.
+     */
    @FXML
    public void addDroneAction(ActionEvent ae){
        Node source = (Node) ae.getSource();
@@ -47,6 +52,11 @@ public class ParameterWindowController{
        isDronePlacement = true;
    }
 
+    /**
+     * Set the next action as a drone removal.
+     *
+     * @param ae The event that has triggered this action.
+     */
    @FXML
    public void removeDroneAction(ActionEvent ae){
        Node source = (Node) ae.getSource();
@@ -57,6 +67,11 @@ public class ParameterWindowController{
        isDroneRemoving = true;
    }
 
+    /**
+     * Set the next action as a charging station placement.
+     *
+     * @param ae The event that has triggered this action.
+     */
    @FXML
    public void addChargingStationAction(ActionEvent ae){
        Node source = (Node) ae.getSource();
@@ -67,6 +82,11 @@ public class ParameterWindowController{
        isChargingStationPlacement = true;
    }
 
+    /**
+     * Set the next action as a charging station removal.
+     *
+     * @param ae The event that has triggered this action.
+     */
    @FXML
    public void removeChargingStationAction(ActionEvent ae){
        Node source = (Node) ae.getSource();
@@ -77,6 +97,11 @@ public class ParameterWindowController{
        isChargingStationRemoving = true;
    }
 
+    /**
+     * Start simulation element placement or removing according to previous action
+     *
+     * @param event The trigger event
+     */
    @FXML
     public void onVisualPaneClicked(MouseEvent event){
        if(isLeftClick(event)) {
@@ -93,27 +118,57 @@ public class ParameterWindowController{
        }
    }
 
+    /**
+     * Launch the simulation
+     *
+     * @param event The trigger event
+     *
+     * @throws IOException The simulation window was not found
+     */
     @FXML
     public void launchSimulationWithCustomParameters(MouseEvent event) throws IOException {
         if(isLeftClick(event)) {
             System.out.println("run simulation");
             if(isAtLeastOneDroneInstanciated()){
-                TryToConfigureSimulationParameters(event);
+                if(isAtLeastOneChargingStationInstanciated()) {
+                    TryToConfigureSimulationParameters(event);
+                } else {
+                    throwZeroChargingStationErrorPopup();
+                }
             } else {
                 throwZeroDroneErrorPopup();
             }
         }
     }
 
-    private void TryToConfigureSimulationParameters(MouseEvent event) throws IOException {
+    /**
+     * Configure simulation parameters before starting it
+     *
+     * @param event The trigger event
+     */
+    private void TryToConfigureSimulationParameters(MouseEvent event) {
         try {
             ConfigureSimulationParameters(event);
             LaunchSimulationWindow(event);
         } catch(IllegalArgumentException e){
-            throwErrorPopup("You must choose a valid competition difficulty");
+            throwNoDifficultyChosenErrorPopup();
         }
     }
 
+    /**
+     * Throw an error popup that signals that at choosing a difficulty is mandatory
+     */
+    private void throwNoDifficultyChosenErrorPopup(){
+        try {
+            throwErrorPopup("You must choose a valid competition difficulty");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Throw an error popup that signals that at least one drone is needed
+     */
     private void throwZeroDroneErrorPopup() {
         try {
             throwErrorPopup("You must add at least one drone!");
@@ -122,11 +177,34 @@ public class ParameterWindowController{
         }
     }
 
+    /**
+     * Throw an error popup that signals that at least one drone is needed
+     */
+    private void throwZeroChargingStationErrorPopup() {
+        try {
+            throwErrorPopup("You must add at least one charging station!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Throw an error popup
+     *
+     * @param errorMsg The string message of the error
+     *
+     * @throws IOException The associated fxml template was not found
+     */
     private void throwErrorPopup(String errorMsg) throws IOException {
         ErrorPopupView errorPopupView = new ErrorPopupView(errorMsg);
         createPopup(errorPopupView.getParent(), ERROR_TITLE);
     }
 
+    /**
+     * Launch the simulation window
+     *
+     * @param event The trigger event.
+     */
     private void LaunchSimulationWindow(MouseEvent event) {
         try {
             SimulationWindowView.init(Simulation.CUSTOM);
@@ -136,17 +214,36 @@ public class ParameterWindowController{
         }
     }
 
+    /**
+     * Configure the simulation parameters according to the view inputs
+     *
+     * @param event The trigger event
+     *
+     * @throws IllegalArgumentException The difficulty was not set
+     */
     private void ConfigureSimulationParameters(MouseEvent event) throws IllegalArgumentException {
         Parent root = ControllerHelper.getRootWith(event);
         ConfigureTimeSimulationParameters(root);
         ConfigureCompetitionDifficultyParameter(root);
     }
 
+    /**
+     * Confiure competion difficulty according to view combobox selection
+     *
+     * @param root The root node of the window
+     *
+     * @throws IllegalArgumentException No choice was made
+     */
     private void ConfigureCompetitionDifficultyParameter(Parent root) throws IllegalArgumentException {
         ComboBox<String> competitionLevels = (ComboBox<String>) root.lookup("#competitionLevelComboBox");
         setCompetitionDifficulty(competitionLevels.getValue());
     }
 
+    /**
+     * Configure simulation duration and iterations according to view sliders
+     *
+     * @param root Root node of the window
+     */
     private void ConfigureTimeSimulationParameters(Parent root) {
         Slider IterationSlider = (Slider)  root.lookup("#iterationSlider");
         Slider simulationDurationSlider = (Slider)  root.lookup("#simulationDurationSlider");
@@ -155,10 +252,30 @@ public class ParameterWindowController{
         Simulation.setTimeSimulationParameters((int) simulationDuration, (int) iterationNumber);
     }
 
+    /**
+     * Checks if at least one drone is instanciated
+     *
+     * @return true if yes
+     */
     private boolean isAtLeastOneDroneInstanciated() {
         return Simulation.getDrones().size() > 0;
     }
 
+    /**
+     * Checks if at least one charging station is instanciated
+     *
+     * @return true if yes
+     */
+    private boolean isAtLeastOneChargingStationInstanciated() {
+        return Simulation.getChargingStations().size() > 0;
+    }
+
+    /**
+     * Create a drone element and add it to the view
+     *
+     * @param event The trigger event
+     * @param root Root node of the window
+     */
     private void createDroneElement(MouseEvent event, Parent root) {
         Slider droneWeightCapacity = (Slider)  root.lookup("#droneWeightCapacity");
         Slider droneBatteryCapacity = (Slider) root.lookup("#droneBatteryCapacity");
@@ -169,6 +286,13 @@ public class ParameterWindowController{
         addDroneElement(event, pane, drone);
     }
 
+    /**
+     * Add a drone graphic element to the window
+     *
+     * @param event Trigger event
+     * @param pane The simulationPane
+     * @param drone The drone to add
+     */
     private void addDroneElement(MouseEvent event, Pane pane, Drone drone) {
         try {
             drone.setCoord(calculateModelCoordinate((float) event.getX()) , calculateModelCoordinate((float) event.getY()));
@@ -184,6 +308,12 @@ public class ParameterWindowController{
         }
     }
 
+    /**
+     * Remove the targeted drone
+     *
+     * @param event The trigger event
+     * @param root The root node of the window
+     */
     private void removeDroneElement(MouseEvent event, Parent root){
         DroneGraphicElement droneGraphic = getDroneInRadius((float) event.getX(), (float) event.getY());
         if(droneGraphic != null) {
@@ -197,6 +327,14 @@ public class ParameterWindowController{
         }
     }
 
+    /**
+     * Get one drone in a radius around the given coords
+     *
+     * @param x abscissa in meters
+     * @param y ordinate in meters
+     *
+     * @return drone in radius if it exists
+     */
     private DroneGraphicElement getDroneInRadius(Float x, Float y) {
        for (DroneGraphicElement drone: getDroneGraphicElements()){
             if(isInRadius(drone.getX(), x) && isInRadius(drone.getY(), y)){
@@ -206,12 +344,25 @@ public class ParameterWindowController{
        return null;
     }
 
+    /**
+     * Create a chargin station element and add it to the view
+     *
+     * @param event The trigger event
+     * @param root Root node of the window
+     */
     private void createChargingStationElement(MouseEvent event, Parent root) {
         Pane pane = (Pane) root.lookup("#visualSettingPane");
         ChargingStation chargingStation = new ChargingStation(chargingStationIdIncrement++);
         addChargingStationElement(event, pane, chargingStation);
     }
 
+    /**
+     * Add a charging station graphic element to the window
+     *
+     * @param event Trigger event
+     * @param pane The simulationPane
+     * @param chargingStation The charging station to add
+     */
     private void addChargingStationElement(MouseEvent event, Pane pane, ChargingStation chargingStation) {
         try {
             chargingStation.setCoord(calculateModelCoordinate((float) event.getX()) , calculateModelCoordinate((float) event.getY()));
@@ -226,6 +377,12 @@ public class ParameterWindowController{
         }
     }
 
+    /**
+     * Remove the targeted charging station
+     *
+     * @param event The trigger event
+     * @param root The root node of the window
+     */
     private void removeChargingStationElement(MouseEvent event, Parent root) {
         ChargingStationGraphicElement chargingStationGraphic = getChargingStationInRadius((float) event.getX(), (float) event.getY());
         if(chargingStationGraphic != null) {
@@ -239,6 +396,14 @@ public class ParameterWindowController{
         }
     }
 
+    /**
+     * Get one charging station in a radius around the given coords
+     *
+     * @param x abscissa in meters
+     * @param y ordinate in meters
+     *
+     * @return charging station in radius if it exists
+     */
     private ChargingStationGraphicElement getChargingStationInRadius(Float x, Float y) {
         for (ChargingStationGraphicElement chargingStation: getChargingStationGraphicElements()){
             if(isInRadius(chargingStation.getX(), x) && isInRadius(chargingStation.getY(), y)){
@@ -248,10 +413,21 @@ public class ParameterWindowController{
         return null;
     }
 
+    /**
+     * Chec if the point is in the radius
+     *
+     * @param x reference point
+     * @param x1 tested point
+     *
+     * @return true f yes
+     */
     private Boolean isInRadius(Float x, Float x1) {
        return (x >= x1 - selectRadius && x <= x1 + selectRadius);
     }
 
+    /**
+     * Reset all placement flags
+     */
     private void resetFlags(){
         isDronePlacement = false;
         isDroneRemoving = false;
