951eb4fd1de33a446e3c6dccc7a0227d228cc766
Rémi Cabezas <remi.cabezas@gmx.com>
2019-06-09 19:40:47 +0200

    Add the ability to change the simulation speed in the simulation window.
    Add commentaries
    Remove some warnings

--- src/main/java/fr/utbm/gl52/droneSimulator/controller/SimulationWindowController.java
+++ src/main/java/fr/utbm/gl52/droneSimulator/controller/SimulationWindowController.java
@@ -1,5 +1,8 @@
 package fr.utbm.gl52.droneSimulator.controller;
 
+import fr.utbm.gl52.droneSimulator.model.Simulation;
+import fr.utbm.gl52.droneSimulator.view.SimulationWindowView;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
@@ -14,5 +17,31 @@ public class SimulationWindowController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    /**
+     * Increase the simulation speed until the limit
+     */
+    @FXML
+    public void onSpeedPlusAction(){
+        Float currentSpeed = Simulation.getSimulationSpeed();
+        Float newSpeed = currentSpeed + 1;
+        if(newSpeed <= Simulation.getMaxSimulationSpeedAcceleration()) {
+            Simulation.setSimulationSpeed(newSpeed);
+            SimulationWindowView.refreshSimulationSpeed();
+        }
+    }
+
+    /**
+     * Decrease the simulation speed until the limit
+     */
+    @FXML
+    public void onSpeedMinusAction(){
+        Float currentSpeed = Simulation.getSimulationSpeed();
+        Float newSpeed = currentSpeed - 1;
+        if(newSpeed >= Simulation.getMinSimulationSpeedAcceleration()) {
+            Simulation.setSimulationSpeed(newSpeed);
+            SimulationWindowView.refreshSimulationSpeed();
+        }
     }
 }
