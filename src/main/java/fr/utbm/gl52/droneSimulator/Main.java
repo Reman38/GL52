291ed4c88c94af34e33a17d6e292cb304b1bf32c
Rémi Cabezas <remi.cabezas@gmx.com>
2019-06-15 17:26:45 +0200

    Revert "rebase on threads"
    This reverts commit 563a5439

---
+++ src/main/java/fr/utbm/gl52/droneSimulator/Main.java
@@ -0,0 +1,21 @@
+package fr.utbm.gl52.droneSimulator;
+
+import fr.utbm.gl52.droneSimulator.service.DbDroneService;
+import fr.utbm.gl52.droneSimulator.service.entity.DbDrone;
+
+public class Main {
+    public static void main(String[] args) {
+        var simulationId = 1;
+
+        var dbDroneService = new DbDroneService();
+
+        var dbDrones = dbDroneService.getAllFromSimulationId(simulationId);
+
+        for (DbDrone dbDrone : dbDrones) {
+            System.out.println(dbDrone.toString());
+        }
+
+//        GuiMain.main(args);
+    }
+
+}
